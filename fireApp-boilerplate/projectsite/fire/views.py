c31887b18448fd39256fd2fa1c3c3f9a1bb0458a
ink3rroll <ink3rroll@gmail.com>
2024-11-14 09:59:57 +0800

    dynamic pie chart

--- fireApp-boilerplate/projectsite/fire/views.py
+++ fireApp-boilerplate/projectsite/fire/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from fire.models import Locations, Incident
+from django.db import connection
+from django.http import JsonResponse
+from django.db.models.functions import ExtractMonth
+from django.db.models import Count
+from datetime import datetime
+
 
 
 class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
+def PieCountbySeverity(request):
+    query = '''
+    SELECT severity_level, COUNT(*) as count
+    FROM fire_incident
+    GROUP BY severity_level
+    '''
+    data = {}
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+    if rows:
+        # Construct the dictionary with severity level as keys and count as values
+        data = {severity: count for severity, count in rows}
+    else:
+        data = {}
+
+    return JsonResponse(data)
